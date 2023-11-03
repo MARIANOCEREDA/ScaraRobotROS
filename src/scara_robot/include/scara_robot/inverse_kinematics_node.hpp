45a33ca9e07462e6bd90b98eb6e0de538da5c178
MARIANOCEREDA <gmarianocereda@gmail.com>
2023-11-02 23:43:39 -0300

    correct inverse kine

--- src/scara_robot/include/scara_robot/inverse_kinematics_node.hpp
+++ src/scara_robot/include/scara_robot/inverse_kinematics_node.hpp
@@ -1,5 +1,6 @@
 #include "rclcpp/rclcpp.hpp"
 #include "example_interfaces/msg/float64_multi_array.hpp"
+#include "trajectory_msgs/msg/joint_trajectory.hpp"
 #include "robot_dimensions.hpp"
 #include <cmath>
 
@@ -10,12 +11,13 @@ class InverseKinematicsNode : public rclcpp::Node
     private:
         rclcpp::Publisher<example_interfaces::msg::Float64MultiArray>::SharedPtr pub_joint;
         rclcpp::Subscription<example_interfaces::msg::Float64MultiArray>::SharedPtr ws_sub_joint;
+        rclcpp::Publisher<trajectory_msgs::msg::JointTrajectory>::SharedPtr pub_joint_state;
         Robot::robot_dimensions robot_dim;
-        float q1, q2, q3;
+        float q[3] = {0.0, 0.0, 0.0};
 
     public:
         InverseKinematicsNode();
         void WsCallback(const example_interfaces::msg::Float64MultiArray::SharedPtr position);
         void PublishJoints();
-        void CalculateInverseKinematics(float x, float y, float z);
+        void CalculateInverseKinematics(float&, float&, float&);
 };
--- src/scara_robot/src/inverse_kinematics_node.cpp
+++ src/scara_robot/src/inverse_kinematics_node.cpp
@@ -3,7 +3,7 @@
 #include "string.h"
 
 #define WS_TOPIC_NAME "/position"
-
+#define JOINT_STATE_TOPIC_NAME "/set_joint_trajectory"
 
 InverseKinematicsNode::InverseKinematicsNode() : Node(NODE_NAME) {
 
@@ -14,6 +14,11 @@ InverseKinematicsNode::InverseKinematicsNode() : Node(NODE_NAME) {
         WS_TOPIC_NAME, 
         10,
         std::bind(&InverseKinematicsNode::WsCallback, this, std::placeholders::_1));
+    
+    pub_joint_state = this->create_publisher<trajectory_msgs::msg::JointTrajectory>(
+        JOINT_STATE_TOPIC_NAME,
+        10
+    );
 }
 
 void InverseKinematicsNode::WsCallback(const example_interfaces::msg::Float64MultiArray::SharedPtr position)
@@ -21,35 +26,60 @@ void InverseKinematicsNode::WsCallback(const example_interfaces::msg::Float64Mul
     float x = position->data[0];
     float y = position->data[1];
     float z = position->data[2];
+    std::string info_msg = "Incomming point: {" + std::to_string(x) + "," + std::to_string(y) + "," + std::to_string(z) + "}";
+    RCLCPP_INFO(this->get_logger(), info_msg.c_str());
     CalculateInverseKinematics(x, y, z);
 }
 
-void InverseKinematicsNode::CalculateInverseKinematics(float px, float py, float pz)
+void InverseKinematicsNode::CalculateInverseKinematics(float& px,float& py, float& pz)
 {
     float num, den, beta, alpha;
 
-    q1 = pz;
+    q[0] = pz;
 
-    num = pow(px, 2) + pow(py, 2) - pow(robot_dim.a1, 2) + pow(robot_dim.a2, 2);
+    num = std::pow(px, 2) + std::pow(py, 2) - std::pow(robot_dim.a1, 2) - std::pow(robot_dim.a2, 2);
     den = 2 * robot_dim.a1 * robot_dim.a2;
 
-    q3 = num / den;
+    q[2] = std::acos(num / den);
 
-    num = robot_dim.a2 * std::sin(q3);
-    den = robot_dim.a1 + robot_dim.a2 * std::cos(q3);
-    alpha = std::acos(num) / (den);
+    num = robot_dim.a2 * std::sin(q[2]);
+    den = robot_dim.a1 + robot_dim.a2 * std::cos(q[2]);
+    alpha = std::atan(num / den);
 
-    beta = std::atan(px / (pow(px,2) + pow(py,2))); 
+    beta = std::atan(px / py); 
 
-    q2 = beta - alpha;
+    q[1] = beta - alpha;
 
-    std::string info_msg = "{" + std::to_string(q1) + "," + std::to_string(q2) + "," + std::to_string(q1);
+    std::string info_msg = "Angles[rad]: {" + std::to_string(q[0]) + "," + std::to_string(q[1]) + "," + std::to_string(q[2]) + "}";
     RCLCPP_INFO(this->get_logger(), info_msg.c_str());
+
+    PublishJoints();
 }
 
 void InverseKinematicsNode::PublishJoints()
 {
+    RCLCPP_INFO(this->get_logger(), "Publishing joints ...");
 
+    auto joint_trajectory_msg = trajectory_msgs::msg::JointTrajectory();
+    joint_trajectory_msg.header.stamp.sec = 0;
+    joint_trajectory_msg.header.stamp.nanosec = 0;
+    joint_trajectory_msg.header.frame_id = "base_link";
+
+    joint_trajectory_msg.joint_names.push_back("joint0");
+    joint_trajectory_msg.joint_names.push_back("joint1");
+    joint_trajectory_msg.joint_names.push_back("joint2");
+    
+    trajectory_msgs::msg::JointTrajectoryPoint point;
+    point.positions.push_back(q[0]); // Posición
+    point.positions.push_back(q[1]); // Posición
+    point.positions.push_back(q[2]); // Posición
+
+    point.time_from_start.sec = 1;   // Tiempo en segundos
+    point.time_from_start.nanosec = 0;
+
+    joint_trajectory_msg.points.push_back(point);
+
+    pub_joint_state->publish(joint_trajectory_msg);
 }
 
 
